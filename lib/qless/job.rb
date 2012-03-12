dcf0d874859b5f14cf4f6cf114012e9972e8a687
Dan Lecocq <dan@seomoz.org>
2012-03-12 12:45:32 -0700

    Including the Gemfile and the initial version of the app.

--- lib/qless/job.rb
+++ lib/qless/job.rb
@@ -4,22 +4,30 @@ require "json"
 
 module Qless
   class Job
-    attr_reader :id, :expires, :state, :queue, :history, :worker
+    attr_reader :id, :expires, :state, :queue, :history, :worker, :retries, :remaining, :failure
     attr_accessor :data, :priority, :tags
     
     def initialize(redis, atts)
-      @redis    = redis
-      @id       = atts['id']            or throw 'Missing ID'
-      @data     = atts['data']          or throw 'Missing user data'
-      @priority = atts['priority'].to_i or throw 'Missing priority'
-      @tags     = atts['tags']          or throw 'Missing tags'
-      @worker   = atts['worker']        or throw 'Missing worker'
-      @expires  = atts['expires'].to_i  or throw 'Missing expires'
-      @state    = atts['state']         or throw 'Missing state'
-      @queue    = atts['queue']         or throw 'Missing queue'
-      @history  = atts['history']       or []
+      @redis     = redis
+      @id        = atts['id']             or throw 'Missing ID'
+      @data      = atts['data']           or throw 'Missing user data'
+      @priority  = atts['priority'].to_i  or throw 'Missing priority'
+      @tags      = atts['tags']           or throw 'Missing tags'
+      @worker    = atts['worker']         or throw 'Missing worker'
+      @expires   = atts['expires'].to_i   or throw 'Missing expires'
+      @state     = atts['state']          or throw 'Missing state'
+      @queue     = atts['queue']          or throw 'Missing queue'
+      @retries   = atts['retries'].to_i   or throw 'Missing retries'
+      @remaining = atts['remaining'].to_i or throw 'Missing remaining'
+      @failure   = atts['failure']        or {}
+      @history   = atts['history']        or []
+      # This is a silly side-effect of Lua doing JSON parsing
+      if @tags == {}
+        @tags = []
+      end
       # Our lua scripts
       @put    = Lua.new('put'   , @redis)
+      @track  = Lua.new('track' , @redis)
       @cancel = Lua.new('cancel', @redis)
     end
     
@@ -52,6 +60,14 @@ module Qless
     
     def cancel()
       return @cancel.call([], [@id])
+    end
+    
+    def track(*tags)
+      return @track.call([], ['track', @id, Time.now().to_i] + tags)
+    end
+    
+    def untrack()
+      return @track.call([], ['untrack'], [@id, Time.now().to_i])
     end
   end  
 end
