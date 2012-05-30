fb803076d4860f32660a313c565f5fc48038c05a
Patrick Roby <proby@seomoz.org>
2012-05-30 13:16:35 -0700

    add integration tests for worker

--- spec/integration/worker_spec.rb
+++ spec/integration/worker_spec.rb
@@ -11,29 +11,43 @@ class WorkerIntegrationJob
 end
 
 describe "Worker integration", :integration do
-  def start_worker
-    unless @child = fork
-      with_env_vars 'REDIS_URL' => redis_url, 'QUEUE' => 'main', 'INTERVAL' => '0.0001' do
-        Qless::Worker.start
-        exit!
+  def initialize_and_start_worker(run_as_single_process)
+    if run_as_single_process
+      unless @child = fork
+        start_worker(run_as_single_process)
       end
+    else
+      start_worker(run_as_single_process)
     end
   end
-  
-  it 'can start a worker and then shut it down' do
-    words = %w{foo bar howdy}
-    start_worker
-    
-    queue = client.queues["main"]
-    words.each do |word|
-      queue.put(WorkerIntegrationJob, "word" => word, "redis_url" => client.redis.client.id)
+
+  def start_worker(run_as_single_process)
+    with_env_vars 'REDIS_URL' => redis_url, 'QUEUE' => 'main', 'INTERVAL' => '0.0001', 'RUN_AS_SINGLE_PROCESS ' => run_as_single_process do
+      Qless::Worker.start
+      exit!
     end
-    
-    # Wait for the job to complete, and then kill the child process
-    words.each do |word|
-      client.redis.brpop('worker_integration_job', 10).should eq(['worker_integration_job', word])
-    end
-    Process.kill("QUIT", @child)
   end
+
+  shared_examples_for 'a running worker' do |run_as_single_process|
+    it 'can start a worker and then shut it down' do
+      words = %w{foo bar howdy}
+      initialize_and_start_worker(run_as_single_process)
+
+      queue = client.queues["main"]
+      words.each do |word|
+        queue.put(WorkerIntegrationJob, "word" => word, "redis_url" => client.redis.client.id)
+      end
+
+      # Wait for the job to complete, and then kill the child process
+      words.each do |word|
+        client.redis.brpop('worker_integration_job', 10).should eq(['worker_integration_job', word])
+      end
+      Process.kill("QUIT", @child)
+    end
+  end
+
+  it_behaves_like 'a running worker', '0'
+
+  it_behaves_like 'a running worker', '1'
 end
 
