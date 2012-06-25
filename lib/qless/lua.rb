88cbd475e31db28acb549b002e6321589085d9db
Patrick Roby <proby@seomoz.org>
2012-06-25 11:41:18 -0700

    make the redis-rb 3.0 upgrade backwards compatible back to redis-rb 2.2

--- lib/qless/lua.rb
+++ lib/qless/lua.rb
@@ -14,10 +14,14 @@ module Qless
     end
     
     def call(keys, argv)
+      # true if using the 2.x version of redis gem
+      use_legacy_evalsha = Gem.loaded_specs['redis'].version < Gem::Version.create('3.0')
       begin
+        return @redis.evalsha(@sha, keys.length, *(keys + argv)) if use_legacy_evalsha
         return @redis.evalsha(@sha, keys: keys, argv: argv)
       rescue
         reload
+        return @redis.evalsha(@sha, keys.length, *(keys + argv)) if use_legacy_evalsha
         return @redis.evalsha(@sha, keys: keys, argv: argv)
       end
     end
