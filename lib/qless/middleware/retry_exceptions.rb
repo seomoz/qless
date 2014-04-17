3ca9a66a3ee82d2a469355e11daf4792ad2ee170
Myron Marston <myron.marston@gmail.com>
2014-04-17 13:47:33 -0700

    Improve RetryExceptions middleware to support exception-dependent backoffs.

--- lib/qless/middleware/retry_exceptions.rb
+++ lib/qless/middleware/retry_exceptions.rb
@@ -17,8 +17,6 @@ module Qless
         attempt_num = (job.original_retries - job.retries_left) + 1
         failure = Qless.failure_formatter.format(job, error)
         job.retry(backoff_strategy.call(attempt_num, error), *failure)
-
-        on_retry_callback.call(error, job)
       end
 
       def retryable_exception_classes
@@ -39,27 +37,19 @@ module Qless
         @backoff_strategy ||= NO_BACKOFF_STRATEGY
       end
 
-      DEFAULT_ON_RETRY_CALLBACK = lambda { |error, job| }
-      def use_on_retry_callback(&block)
-        @on_retry_callback = block if block
-      end
-
-      def on_retry_callback
-        @on_retry_callback ||= DEFAULT_ON_RETRY_CALLBACK
-      end
-
-      # If `factor` is omitted it is set to `delay_seconds` to reproduce legacy
-      # behavior.
-      def exponential(delay_seconds, options={})
-        factor = options.fetch(:factor, delay_seconds)
+      def exponential(base, options = {})
         fuzz_factor = options.fetch(:fuzz_factor, 0)
 
-        lambda do |retry_no, error|
-          unfuzzed = delay_seconds * factor**(retry_no - 1)
-          return unfuzzed if fuzz_factor.zero?
-          r = 2 * rand  - 1
-          # r is uniformly distributed in range [-1, 1]
-          unfuzzed * (1 + fuzz_factor * r)
+        lambda do |num, _error|
+          unfuzzed = base**num
+
+          fuzz = 0
+          unless fuzz_factor.zero?
+            max_fuzz = unfuzzed * fuzz_factor
+            fuzz = rand(max_fuzz) * [1, -1].sample
+          end
+
+          unfuzzed + fuzz
         end
       end
     end
